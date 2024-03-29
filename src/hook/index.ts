1905a5ac4b190a1f6089afb7a778ea1cf71d05a9
khaoxkongz <thanapat.makkawal@gmail.com>
2024-03-29 11:25:32 +0700

    feat: add job hooks
    Add addJob, deleteJob and updateJob hooks

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,9 +1,38 @@
 import { JobLoaderProps } from '../interfaces/hook'
-import { JobsDto } from '../interfaces/job.dto'
+import { ICreateJobDto, IUpdateJobDto, JobsDto } from '../interfaces/job.dto'
 
 // Retrieve Job from server
 export const jobLoader = async ({ params }: JobLoaderProps): Promise<JobsDto> => {
   const res = await fetch(`/api/jobs/${params.id}`)
   const data = await res.json()
   return data
+}
+
+// Add New Job
+export const addJob = async (newJob: ICreateJobDto): Promise<void> => {
+  await fetch('/api/jobs', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newJob),
+  })
+}
+
+// Delete Job
+export const deleteJob = async (id: string): Promise<void> => {
+  await fetch(`/api/jobs/${id}`, {
+    method: 'DELETE',
+  })
+}
+
+// Update Job
+export const updateJob = async (job: IUpdateJobDto): Promise<void> => {
+  await fetch(`/api/jobs/${job.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(job),
+  })
 }
