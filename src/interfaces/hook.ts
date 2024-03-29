68628fb7d33ded1b76238aa1a6e669f9ba6bf047
khaoxkongz <thanapat.makkawal@gmail.com>
2024-03-29 11:03:29 +0700

    feat: add job listing interfaces
    Define interfaces for JobListingProps, JobListingsProps, and JobLoaderProps

---
+++ src/interfaces/hook.ts
@@ -0,0 +1,11 @@
+import { Params } from 'react-router-dom'
+import { JobsDto } from './job.dto'
+
+export interface JobLoaderProps {
+  params: Params<string>
+}
+
+export interface UseJobListingsHook {
+  jobs: JobsDto[] | null
+  loading: boolean
+}
