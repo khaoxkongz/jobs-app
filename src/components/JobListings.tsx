2fe6adebc078105c3d61115e63e3ebca971177e9
khaoxkongz <thanapat.makkawal@gmail.com>
2024-03-29 11:06:17 +0700

    feat: job listings component
    Implement JobListings component to render a list of job listings with loading state

---
+++ src/components/JobListings.tsx
@@ -0,0 +1,29 @@
+import Spinner from './Spinner'
+import JobListing from './JobListing'
+import useJobListings from '../hook/useJobListings'
+
+import { JobListingsProps } from '../interfaces/component'
+
+const JobListings = ({ isHome = false }: JobListingsProps) => {
+  const { jobs, loading } = useJobListings({ isHome })
+
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+        </h2>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs && jobs.map((job) => <JobListing key={job.id} job={job} />)}
+          </div>
+        )}
+      </div>
+    </section>
+  )
+}
+
+export default JobListings
